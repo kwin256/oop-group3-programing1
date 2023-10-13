70b765025bb5eb775ffeed26571df88cbad01d0f
kwin256 <kwin256@gmail.com>
2023-10-12 23:32:48 -0600

    代码及流程图整合

---
+++ keith/gas.py
@@ -0,0 +1,19 @@
+gas = "G"
+oil = "O"
+num_hypen = 50
+banner = "-" * num_hypen
+message = "Welcome to Gas Station Supplier Program!"
+full_banner = f"{banner}\n {'*' * 3} {message} {'*' * 3}\n{banner} "
+print(full_banner)
+
+selection = input("Please select the type of purchase: \n G:Gas\n O:Oil\n >>> " ).upper()
+number_of_liter = float(input(" Please enter the number of litres of gas:"))
+gas_price= float(number_of_liter * 1.07)
+
+if number_of_liter > 4000:
+    discount = gas_price * 0.9
+    print(f"the total price of gasoline is {discount}")
+else:
+    print(f"the total price of gasoline is {gas_price}")
+
+
--- xuekun/CalculateTotalPrice.py
+++ xuekun/CalculateTotalPrice.py
@@ -1,5 +1,3 @@
-import sys
-
 # print menu
 print("-------------------------------------------------")
 print("*** Welcome to Gas Station Supplier Program! ***")
@@ -14,61 +12,59 @@ isGasOption = purchaseType == "g" or purchaseType == "G"
 # invalid option check
 if not isOilOption and not isGasOption:
     print("Invalid input, you should enter g/G or o/O")
-
-    # if this is not permit, pls use else to contain content below
-    sys.exit()
-
-# order detail variables
-product = "demo"
-litres = 0.0
-price_before = 0.0
-price_after = 0.0
-
-if isOilOption:
-    # calculate oil price -- zeo to do
-    product = "Oil"
-    cases = float(input("Enter # of cases of Oil: "))
-    litres = cases * 12
-    original_cost_per_litre = 1.27
-    price_before = litres * original_cost_per_litre
-    if cases > 8:
-        price_after = price_before * 0.9
-    else:
-        price_after = price_before
 else:
-    # calculate gas price -- keith to do
-    product = "Gas"
-    print("")
+    if isOilOption:
+        # calculate oil price -- zeo to do
+        product = "Oil"
+        cases = float(input("Enter # of cases of Oil: "))
+        litres = cases * 12
+        original_cost_per_litre = 1.27
+        price_before = litres * original_cost_per_litre
+        if cases > 8:
+            price_after = price_before * 0.9
+        else:
+            price_after = price_before
+    else:
+        # calculate gas price -- keith to do
+        product = "Gas"
+        litres = float(input(" Please enter the number of litres of gas:"))
+        price_before = float(litres * 1.07)
 
-# calculate gst and total price
-provinceAbbr = input("Please enter the 2 letters province abbreviation: ")
-provinceAbbr = provinceAbbr.upper()
-match provinceAbbr:
-    case "AB":
-        gst = 0.05
-    case "BC":
-        gst = 0.05
-    case "MB":
-        gst = 0.05
-    case "NT":
-        gst = 0.05
-    case "NU":
-        gst = 0.05
-    case "QC":
-        gst = 0.05
-    case "SK":
-        gst = 0.05
-    case "yt":
-        gst = 0.05
-    case "ON":
-        gst = 0.13
-    case others:
-        gst = 0.15
-gst_money = round(price_after * gst, 2)
-total_price = price_after + gst_money
+        if litres > 4000:
+            price_after = price_before * 0.9
+        else:
+            price_after = price_before
 
-print("---------------------------------------------------------------------------------")
-print("Product      # Of Litres    Price Before    Price After    GST    Total Price")
-print(f"{product}      {litres}         {price_before}            {price_after}           {gst_money}   {total_price}")
-print("---------------------------------------------------------------------------------")
-print("Thanks for your business, Good Bye")
+    # calculate gst and total price
+    provinceAbbr = input("Please enter the 2 letters province abbreviation: ")
+    provinceAbbr = provinceAbbr.upper()
+    match provinceAbbr:
+        case "AB":
+            gst = 0.05
+        case "BC":
+            gst = 0.05
+        case "MB":
+            gst = 0.05
+        case "NT":
+            gst = 0.05
+        case "NU":
+            gst = 0.05
+        case "QC":
+            gst = 0.05
+        case "SK":
+            gst = 0.05
+        case "yt":
+            gst = 0.05
+        case "ON":
+            gst = 0.13
+        case others:
+            gst = 0.15
+    gst_money = round(price_after * gst, 2)
+    total_price = price_after + gst_money
+
+    print("---------------------------------------------------------------------------------")
+    print("Product      # Of Litres    Price Before    Price After    GST    Total Price")
+    print(
+        f"{product}      {litres}         {price_before}            {price_after}           {gst_money}   {total_price}")
+    print("---------------------------------------------------------------------------------")
+    print("Thanks for your business, Good Bye")
