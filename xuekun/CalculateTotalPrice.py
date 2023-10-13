eb1ede023109022ee0cd743929004b4558dae535
kwin256 <kwin256@gmail.com>
2023-10-12 18:13:11 -0600

    优化代码

--- xuekun/CalculateTotalPrice.py
+++ xuekun/CalculateTotalPrice.py
@@ -26,12 +26,13 @@ price_after = 0.0
 
 if isOilOption:
     # calculate oil price -- zeo to do
+    product = "Oil"
     cases = float(input("Enter # of cases of Oil: "))
-    litres = cases*12
+    litres = cases * 12
     original_cost_per_litre = 1.27
-    price_before = litres*original_cost_per_litre
-    if(cases > 8):
-        price_after = price_before*0.9
+    price_before = litres * original_cost_per_litre
+    if cases > 8:
+        price_after = price_before * 0.9
     else:
         price_after = price_before
 else:
