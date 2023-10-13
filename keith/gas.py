25f6e8d3a4ea27000d2513c13a7237b69e8de315
kwin256 <kwin256@gmail.com>
2023-10-13 14:11:15 -0600

    testing file

--- keith/gas.py
+++ keith/gas.py
@@ -6,14 +6,12 @@ message = "Welcome to Gas Station Supplier Program!"
 full_banner = f"{banner}\n {'*' * 3} {message} {'*' * 3}\n{banner} "
 print(full_banner)
 
-selection = input("Please select the type of purchase: \n G:Gas\n O:Oil\n >>> " ).upper()
+selection = input("Please select the type of purchase: \n G:Gas\n O:Oil\n >>> ").upper()
 number_of_liter = float(input(" Please enter the number of litres of gas:"))
-gas_price= float(number_of_liter * 1.07)
+gas_price = float(number_of_liter * 1.07)
 
 if number_of_liter > 4000:
     discount = gas_price * 0.9
     print(f"the total price of gasoline is {discount}")
 else:
     print(f"the total price of gasoline is {gas_price}")
-
-
